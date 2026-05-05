b069db635fc8c8f9c655b1ba35984d1cf4d522e6
HostVoid <thebadnix@gmail.com>
2026-05-06 01:53:58 +0400

    add spicetify

---
+++ home/cicada/programs/GUI/spicetify/default.nix
@@ -0,0 +1,14 @@
+{ inputs, pkgs, ... }: {
+    extraSpecialArgs = { inherit inputs; };
+    users."cicada" = { config, ... }: {
+      imports = [ inputs.spicetify-nix.homeManagerModules.default ];
+
+      programs.spicetify = {
+        enable = true;
+        enabledExtensions = with inputs.spicetify-nix.legacyPackages.${pkgs.system}.extensions; [
+          adblock
+          hidePodcasts
+        ];
+      };
+    };
+}
