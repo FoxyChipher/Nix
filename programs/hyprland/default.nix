b416ea9b79848883a9d3ebee0c9d04ac73bfd5a0
HostVoid <thebadnix@gmail.com>
2026-05-07 21:44:09 +0400

    add hyprland

---
+++ programs/hyprland/default.nix
@@ -0,0 +1,15 @@
+{ inputs, pkgs, config, ... }: {
+  programs.hyprland = {
+    enable = true;
+    withUWSM = true; # recommended for most users
+    xwayland.enable = true; # Xwayland can be disabled.
+  };
+  wayland.windowManager.hyprland.systemd.enable = false;
+  xdg.portal = {
+    enable = true;
+    extraPortals = with pkgs; [ xdg-desktop-portal-hyprland ];
+  };
+  # wayland.windowManager.hyprland.plugins = [
+  #   pkgs.hyprlandPlugins.PLUGIN_NAME
+  # ];
+}
