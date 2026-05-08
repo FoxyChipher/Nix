095179008108bee1463ffe12b31dda9137790503
cicada <thebadnix@gmail.com>
2026-05-08 23:59:40 +0400

    rebuild

--- home/cicada/programs/GUI/zed/default.nix
+++ home/cicada/programs/GUI/zed/default.nix
@@ -1,6 +1,4 @@
-{
-  ...
-}:
+{ ... }:
 {
   # home.packages = [ pkgs.zed-editor ];
 
@@ -11,49 +9,49 @@
 
   programs.zed-editor = {
     enable = true;
-    # extensions = [
-    #   "nix"
-    #   "toml"
-    #   "rust"
-    #   "html"
-    #   "java"
-    #   "php"
-    #   "sql"
-    #   "dockerfile"
-    #   "lua"
-    #   "c#"
-    #   "nginx"
-    # ];
-    # userSettings = {
-    #   theme = {
-    #     mode = "dark";
-    #     dark = "GitHub Dark Tritanopia";
-    #     light = "One Light";
-    #   };
-    #   icon_theme = "Zed (Default)";
-    #   hour_format = "hour24";
-    #   telemetry = {
-    #     diagnostics = false;
-    #     metrics = false;
-    #   };
-    #   minimap = {
-    #     show = "always";
-    #   };
-    #   vim_mode = false;
-    #   languages = {
-    #     Nix = {
-    #       tab_size = 2;
-    #     };
-    #   };
-    #   git_panel = {
-    #     dock = "right";
-    #   };
-    #   tabs = {
-    #     git_status = false;
-    #     file_icons = true;
-    #     activate_on_close = "neighbour";
-    #   };
-    #   # proxy = "";
-    # };
+    extensions = [
+      "nix"
+      "toml"
+      "rust"
+      "html"
+      "java"
+      "php"
+      "sql"
+      "dockerfile"
+      "lua"
+      "c#"
+      "nginx"
+    ];
+    userSettings = {
+      theme = {
+        mode = "dark";
+        dark = "GitHub Dark Tritanopia";
+        light = "One Light";
+      };
+      icon_theme = "Zed (Default)";
+      hour_format = "hour24";
+      telemetry = {
+        diagnostics = false;
+        metrics = false;
+      };
+      minimap = {
+        show = "always";
+      };
+      vim_mode = false;
+      # languages = {
+      #   Nix = {
+      #     tab_size = 2;
+      #   };
+      # };
+      git_panel = {
+        dock = "right";
+      };
+      tabs = {
+        git_status = false;
+        file_icons = true;
+        activate_on_close = "neighbour";
+      };
+      # proxy = "";
+    };
   };
 }
