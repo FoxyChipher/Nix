ea37d80e18ae3ea10c37975c3b9e76d40a78782f
cicada <thebadnix@gmail.com>
2026-05-10 19:15:06 +0400

    add gtk

---
+++ home/cicada/programs/other/gtk/default.nix
@@ -0,0 +1,20 @@
+{ pkgs, ... }: {
+  gtk = {
+    enable = true;
+    # theme = {
+    #   name = "Adwaita-dark";
+    #   package = pkgs.gnome-themes-extra;
+    # };
+    # iconTheme = {
+    #   name = "Papirus-Dark";
+    #   package = pkgs.papirus-icon-theme;
+    # };
+    gtk3.extraConfig = {
+      gtk-application-prefer-dark-theme = 1;
+      gtk-decoration-layout = "menu:";
+    };
+    gtk4.extraConfig = {
+      gtk-application-prefer-dark-theme = 1;
+    };
+  };
+}
